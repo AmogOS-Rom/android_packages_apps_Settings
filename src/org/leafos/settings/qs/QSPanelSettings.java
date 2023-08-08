ac37da85e389d47eacff8e6b025f3c0c41aff0e2
Davigamer987 <davimarchesiprimo@gmail.com>
2023-08-08 17:34:13 -0300

    Settings: Likely broken A11 QS Implementation

--- src/org/leafos/settings/qs/QSPanelSettings.java
+++ src/org/leafos/settings/qs/QSPanelSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2023 The LeafOS Project
+ * Copyright (C) 2016-2022 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,34 +13,297 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.rising.settings.fragments;
 
-package org.leafos.settings.qs;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
-import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-@SearchIndexable
-public class QSPanelSettings extends DashboardFragment {
+import com.rising.settings.preferences.SystemSettingListPreference;
+import com.rising.settings.preferences.SystemSettingSwitchPreference;
+import com.rising.settings.preferences.SystemSettingEditTextPreference;
 
-    private static final String TAG = "QSPanelSettings";
+import com.android.internal.util.rising.ThemeUtils;
+import com.android.internal.util.rising.systemUtils;
+
+import java.util.List;
+
+@SearchIndexable
+public class UserInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String TAG = "UserInterface";
+
+    private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+    private static final String KEY_QS_UI_STYLE  = "qs_ui_style";
+    private static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+    private static final String SETTINGS_HEADER_IMAGE = "settings_header_image";
+    private static final String SETTINGS_HEADER_IMAGE_RANDOM = "settings_header_image_random";
+    private static final String SETTINGS_HEADER_TEXT = "settings_header_text";
+    private static final String SETTINGS_HEADER_TEXT_ENABLED = "settings_header_text_enabled";
+    private static final String SETTINGS_CONTEXTUAL_MESSAGES = "settings_contextual_messages";
+    private static final String USE_STOCK_LAYOUT = "use_stock_layout";
+    private static final String ABOUT_PHONE_STYLE = "about_card_style";
+    private static final String HIDE_USER_CARD = "hide_user_card";
+
+    private SystemSettingListPreference mQsStyle;
+    private SystemSettingListPreference mQsUI;
+    private ThemeUtils mThemeUtils;
+    private Handler mHandler;
+    private SystemSettingListPreference mSettingsDashBoardStyle;
+    private SystemSettingListPreference mAboutPhoneStyle;
+    private SystemSettingSwitchPreference mUseStockLayout;
+    private SystemSettingSwitchPreference mHideUserCard;
+    private Preference mSettingsHeaderImage;
+    private Preference mSettingsHeaderImageRandom;
+    private Preference mSettingsMessage;
+    private SystemSettingEditTextPreference mSettingsHeaderText;
+    private SystemSettingSwitchPreference mSettingsHeaderTextEnabled;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.rising_settings_ui);
+
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mThemeUtils = new ThemeUtils(getActivity());
+
+        mQsStyle = (SystemSettingListPreference) findPreference(KEY_QS_PANEL_STYLE);
+        mQsUI = (SystemSettingListPreference) findPreference(KEY_QS_UI_STYLE);
+        mCustomSettingsObserver.observe();
+
+        mSettingsDashBoardStyle = (SystemSettingListPreference) findPreference(SETTINGS_DASHBOARD_STYLE);
+        mSettingsDashBoardStyle.setOnPreferenceChangeListener(this);
+        mSettingsHeaderImageRandom = findPreference(SETTINGS_HEADER_IMAGE_RANDOM);
+        mSettingsHeaderImageRandom.setOnPreferenceChangeListener(this);
+        mSettingsMessage = findPreference(SETTINGS_CONTEXTUAL_MESSAGES);
+        mSettingsMessage.setOnPreferenceChangeListener(this);
+        mSettingsHeaderImage = findPreference(SETTINGS_HEADER_IMAGE);
+        mSettingsHeaderImage.setOnPreferenceChangeListener(this);
+        mUseStockLayout = (SystemSettingSwitchPreference) findPreference(USE_STOCK_LAYOUT);
+        mUseStockLayout.setOnPreferenceChangeListener(this);
+        mAboutPhoneStyle = (SystemSettingListPreference) findPreference(ABOUT_PHONE_STYLE);
+        mAboutPhoneStyle.setOnPreferenceChangeListener(this);
+        mHideUserCard = (SystemSettingSwitchPreference) findPreference(HIDE_USER_CARD);
+        mHideUserCard.setOnPreferenceChangeListener(this);
+        mSettingsHeaderText = (SystemSettingEditTextPreference) findPreference(SETTINGS_HEADER_TEXT);
+        mSettingsHeaderText.setOnPreferenceChangeListener(this);
+        mSettingsHeaderTextEnabled = (SystemSettingSwitchPreference) findPreference(SETTINGS_HEADER_TEXT_ENABLED);
+        mSettingsHeaderTextEnabled.setOnPreferenceChangeListener(this);
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_UI_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SETTINGS_DASHBOARD_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_STYLE))) {
+                updateQsStyle(false /*QS UI theme*/);
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_UI_STYLE))) {
+                updateQsStyle(true /*QS UI theme*/);
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.SETTINGS_DASHBOARD_STYLE))) {
+                updateSettingsStyle();
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	Context mContext = getActivity().getApplicationContext();
+	ContentResolver resolver = mContext.getContentResolver();
+        if (preference == mQsStyle) {
+            mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mQsUI) {
+            mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mSettingsDashBoardStyle) {
+            mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mUseStockLayout) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mHideUserCard) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mAboutPhoneStyle) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderImage) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderImageRandom) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsMessage) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderTextEnabled) {
+            boolean enable = (Boolean) newValue;
+            SystemProperties.set("persist.sys.settings.header_text_enabled", enable ? "true" : "false");
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderText) {
+            String value = (String) newValue;
+            SystemProperties.set("persist.sys.settings.header_text", value);
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+	}
+        return false;
+    }
+
+    private void updateQsStyle(boolean isQsUI) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean isA11Style = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_UI_STYLE , 1, UserHandle.USER_CURRENT) == 1;
+	if (isQsUI) {
+	    setQsStyle(isA11Style ? "com.android.system.qs.ui.A11" : "com.android.systemui");
+	} else {
+        int qsPanelStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_PANEL_STYLE , 0, UserHandle.USER_CURRENT);
+
+        switch (qsPanelStyle) {
+            case 0:
+              setQsStyle("com.android.systemui");
+              break;
+            case 1:
+              setQsStyle("com.android.system.qs.outline");
+              break;
+            case 2:
+            case 3:
+              setQsStyle("com.android.system.qs.twotoneaccent");
+              break;
+            case 4:
+              setQsStyle("com.android.system.qs.shaded");
+              break;
+            case 5:
+              setQsStyle("com.android.system.qs.cyberpunk");
+              break;
+            case 6:
+              setQsStyle("com.android.system.qs.neumorph");
+              break;
+            case 7:
+              setQsStyle("com.android.system.qs.reflected");
+              break;
+            case 8:
+              setQsStyle("com.android.system.qs.surround");
+              break;
+            case 9:
+              setQsStyle("com.android.system.qs.thin");
+              break;
+            case 10:
+              setQsStyle("com.android.system.qs.twotoneaccenttrans");
+              break;
+            default:
+              break;
+        }
+
+        }
+    }
+
+    public void setQsStyle(String overlayName) {
+        boolean isA11Style = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_UI_STYLE , 1, UserHandle.USER_CURRENT) == 1;
+        mThemeUtils.setOverlayEnabled(isA11Style ? "android.theme.customization.qs_ui" : "android.theme.customization.qs_panel", overlayName, "com.android.systemui");
+    }
+
+
+    private void updateSettingsStyle() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        int settingsPanelStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SETTINGS_DASHBOARD_STYLE, 0, UserHandle.USER_CURRENT);
+
+        switch (settingsPanelStyle) {
+            case 0:
+              setSettingsStyle("com.android.settings");
+              break;
+            case 1:
+              setSettingsStyle("com.android.system.settings.rui");
+              break;
+            case 2:
+              setSettingsStyle("com.android.system.settings.arc");
+              break;
+            case 3:
+              setSettingsStyle("com.android.system.settings.aosp");
+              break;
+            case 4:
+              setSettingsStyle("com.android.system.settings.mt");
+              break;
+            case 5:
+              setSettingsStyle("com.android.system.settings.card");
+              break;
+            default:
+              break;
+        }
+    }
+
+    public void setSettingsStyle(String overlayName) {
+       mThemeUtils.setOverlayEnabled("android.theme.customization.icon_pack.settings", overlayName, "com.android.settings");
+    }
 
     @Override
     public int getMetricsCategory() {
-        return METRICS_CATEGORY_UNKNOWN;
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
     }
 
-    @Override
-    protected String getLogTag() {
-        return TAG;
-    }
-
-    @Override
-    protected int getPreferenceScreenResId() {
-        return R.xml.qs_panel_settings;
-    }
-
+    /**
+     * For search
+     */
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.qs_panel_settings);
+            new BaseSearchIndexProvider(R.xml.rising_settings_ui) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    return keys;
+                }
+            };
 }
